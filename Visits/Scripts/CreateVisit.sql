3bd07dfbdcfc76220b110483d00815fe7425d1c6
authentiano <2400810220@std.kyu.ac.ug>
2026-07-31 09:56:59 +0300

    Updated the visit script

--- Visits/Scripts/CreateVisit.sql
+++ Visits/Scripts/CreateVisit.sql
@@ -3,12 +3,15 @@ CREATE TABLE Visits
     VisitId INT GENERATED ALWAYS AS IDENTITY,
     VisitNo TEXT NOT NULL,
     PatientId TEXt NOT NULL,
-    MedicalServicesCode TEXT NOT NULL,
+    MedicalId TEXT NOT NULL,
     VisitDate DATETIME NOT NULL,
     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
-    PRIMARY KEY(id),
+    PRIMARY KEY(VisitId),
     CONSTRAINT fk_patient 
         FOREIGN KEY (PatientId) 
-        REFERENCES Patients(PatientId)
+        REFERENCES Patients(PatientId),
+    CONSTRAINT fk_medical 
+        FOREIGN KEY (MedicalId) 
+        REFERENCES Medical_services(MedicalId)
     
 )
